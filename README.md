a6624024b675702cc6ef53dd277a4e649a6dca92
Andre Wolokita <andrewolokita@gmail.com>
2014-11-09 21:31:16 +1100

    pkgmodmake can now find pkg.json files with a tag
    Signed-off-by: Andre Wolokita <andrewolokita@gmail.com>

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ addin-dev
 =========
 
 BR2 config parsing, JSON serialization, config module creation, etc.
+
+This project provides a set of python scripts to convert buildroot Config.in
+files into JSON structures, add tags to the structures and create groups of buildroot config options
+
+package_walk.py handles conversions
+tags.py does tags stuff
--- pkgmodmake.py
+++ pkgmodmake.py
@@ -1,14 +1,25 @@
-# Create package ball
+# Create buildroot package module from tag
+# TODO Multi-tag?
 # (c) 2014 Andre Wolokita
 
 from br2pkg import br2pkg
 from br2pkgmod import br2pkgmod
 from common import env_info
 import json
+import sys
+import os
+import fnmatch
+import re
+env = env_info()
 
-#json_data = open('net.modconfig')
+#json_data = open('net.modconfig
 
+if len(sys.argv) < 2:
+    print 'Not enough args'
+    sys.exit()
 
+tag = sys.argv[1]
+pkgdir = env.brPath + '/../package'
 
 # Convert JSON to strings from unicode or whatever
 def convert(input): 
@@ -21,7 +32,24 @@ def convert(input):
     else:
         return input
 
-data = json.load(json_data)
+def tag_check(pat,line):
+"""match give tag in data.tags"""
+    for tags in line:
+        if re.match(pat,tags):
+            return 1
+    return 0
+
+
+for subdir, dirs, files in os.walk(pkgdir):
+    for file in files:
+        if fnmatch.fnmatch(file, 'pkg.json'):
+            pkg = br2pkg();
+            json_data = open(os.path.join(subdir, file))
+            data = json.load(json_data)
+            if not tag_check(tag,data["tags"]):
+                continue
+	    print 'found: ' + data["config"]
+#data = json.load(json_data)
 #data2 = convert(data)
 
 
